63f0093a587114e43c06a55a4eb91fd23e037e78
druckdev <druckdev@protonmail.com>
2021-09-16 03:38:25 +0200

    Transition to a (X)event loop

--- texify.c
+++ texify.c
@@ -3,6 +3,7 @@
 
 #include <X11/cursorfont.h> // XC_pencil
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>      // XLookupString(), XK_Escape (X11/keysymdef.h)
 
 int
 main(int argc, char** argv)
@@ -19,6 +20,12 @@ main(int argc, char** argv)
 	Window win;
 	win = XCreateSimpleWindow(dpy, root, 100, 100, 100, 100, 10, black, white);
 
+	// Listen to key and button presses and cursor dragging (move while click)
+	XSelectInput(dpy, win, KeyPressMask | ButtonPressMask | Button1MotionMask);
+	// Listen for WM_DELETE_WINDOW message
+	Atom wm_delete_msg = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
+	XSetWMProtocols(dpy, win, &wm_delete_msg, 1);
+
 	// Display the window
 	XMapWindow(dpy, win);
 
@@ -28,25 +35,27 @@ main(int argc, char** argv)
 	// Sync
 	XSync(dpy, 0);
 
-	int x, y;
-	unsigned int mask;
+	// event loop
+	XEvent event;
+	while (!XNextEvent(dpy, &event)) {
 
-	while (1) {
-		// Throw-away, cannot pass NULL
-		int i;
-		Window w;
+		if (event.type == KeyPress) {
+			if (XLookupKeysym(&event.xkey, 0) == XK_Escape) {
+				break;
+			}
+		} else if (event.type == ButtonPress) {
+			if (event.xbutton.button == Button3) {
+				break;
+			}
+		} else if (event.type == ClientMessage) {
+			if (event.xclient.data.l[0] == wm_delete_msg) {
+				break;
+			}
+		} else if (event.type == MotionNotify) {
+			XMotionEvent xmotion = event.xmotion;
+			printf("x: %d, y: %d\n", xmotion.x, xmotion.y);
+		}
 
-		// Get pointer location
-		XQueryPointer(dpy, win, &w, &w, &i, &i, &x, &y, &mask);
-		// printf("x: %d, y: %d, mask: %u\n", x, y, mask);
-
-		int left_click  = mask & (1 << 8);
-		int right_click = mask & (1 << 10);
-		if (left_click)
-			printf("x: %d, y: %d\n", x, y);
-
-		if (right_click)
-			break;
 	}
 
 	XCloseDisplay(dpy);
